7c58af96b17bd53430fd38d77386d80faabbb9d6
rusdec <ruskidecko@gmail.com>
2018-04-02 20:31:39 +0300

    sign in testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@email.ru"
+  end
+
   factory :user do
-    
+    email
+    password 'Qwerty123'    
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -13,17 +13,17 @@ feature 'Signing in', %q{
     visit '/sign_in'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Sign In'
 
-    expect(page).to have_content 'You are welcome!'
+    click_on 'Sign in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'Signing in with incorrect data' do
     visit '/sign_in'
     fill_in 'Email', with: nil
     fill_in 'Password', with: user.password
-    click_on 'Sign In'
+    click_on 'Sign in'
 
-    expect(page).to have_content 'Error!'
+    expect(page).to have_content 'Invalid Email or password.'
   end
 end
