fd72e6fc0f21e8f0eeed2e81121f7d21c2fa6e91
rusdec <ruskidecko@gmail.com>
2018-04-11 17:11:18 +0300

    refactoring: remove contexts from models specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validations' do
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 
-    it do
-      should validate_length_of(:password).
-        is_at_least(5).is_at_most(20)
-    end
+  it do
+    should validate_length_of(:password).
+      is_at_least(5).is_at_most(20)
   end
 
-  context 'relations' do
-    it { should have_many(:questions).dependent(:destroy) }
-    it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:questions).dependent(:destroy) }
+  it { should have_many(:answers).dependent(:destroy) }
+
+  let(:user) { create(:user_with_questions) }
+  let(:question) { user.questions.last }
+
+  it 'user should be an author of given question' do
+    expect(user).to be_author_of(question)
   end
 
-  context 'open interface' do
-    let(:user) { create(:user_with_questions) }
-    let(:question) { user.questions.last }
-
-    let(:second_user_question) do
-      create(:user_with_questions).questions.last
-    end
-
-    it 'user should be an author of given question' do
-      expect(user).to be_author_of(question)
-    end
-
-    it 'user should not be an author of given question' do
-      expect(user.author_of?(second_user_question)).to eq false
-    end
+  it 'user should not be an author of given question' do
+    second_user_question = create(:user_with_questions).questions.last
+    expect(user).to_not be_author_of(second_user_question)
   end
 end
