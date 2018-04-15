9fb1bdb8efee8b44ced00ac5731f0c8e38393cbe
rusdec <ruskidecko@gmail.com>
2018-04-16 01:39:14 +0300

    refactoring: "question uodate" feature testing

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,8 @@
 module QuestionsHelper
   def question_remote_links(question)
-    if current_user&.author_of?(question) 
-      content_tag :div, class: 'question-remote-links' do
-        concat(link_to 'Edit', '#', class: 'link-edit-question')
-        concat(link_to 'Delete', question_path(question), method: :delete)
-      end
+    content_tag :div, class: 'question-remote-links' do
+      concat(link_to 'Edit', '#', class: 'link-edit-question')
+      concat(link_to 'Delete', question_path(question), method: :delete)
     end
   end
 end
