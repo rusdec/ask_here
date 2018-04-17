11b5b5cadd116ab08137e97dbe75dff0515467c2
rusdec <ruskidecko@gmail.com>
2018-04-17 16:14:00 +0300

    migration: add best row to answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404163506) do
+ActiveRecord::Schema.define(version: 20180417130722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180404163506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "best", default: false
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
