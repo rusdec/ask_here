cc8f20ed43b733e3e06945d92b4b0797e2bcabfc
rusdec <ruskidecko@gmail.com>
2018-04-18 13:32:00 +0300

    refactoring: one test one expect

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -11,29 +11,32 @@ RSpec.describe Answer, type: :model do
       is_at_least(10).is_at_most(1000)
   end
 
-  describe 'best unswer' do
+  describe 'best answer' do
     let(:question) { create(:user_with_question_and_best_answer).questions.last }
+    let(:best_answer) { question.best_answers.last }
 
-    it 'uncheckable' do
-      best_answer = question.best_answers.last
-      
-      expect {
+    describe 'when becomes not best answer' do
+      it 'count of best answers decreases' do
+        expect {
+          best_answer.not_best!
+        }.to change(question.best_answers, :count).by(-1)
+      end
+
+      it 'are not longer the best' do
         best_answer.not_best!
-      }.to change(question.best_answers, :count).by(-1)
-
-      expect(best_answer).to_not be_best
+        expect(best_answer).to_not be_best
+      end
     end
 
     it 'may be only one per question' do
       answer = question.answers.find_by(best: false)
+
       expect {
         answer.best!
       }.to_not change(question.best_answers, :count)
     end
 
     it 'first' do
-      best_answer = question.best_answers.last
-
       expect(question.answers.first).to eq(best_answer)
     end
   end
