ae36643229000b3f359251417413d40a9b906f9d
rusdec <ruskidecko@gmail.com>
2018-04-18 14:03:08 +0300

    questions feature tests - minimize using css selector

--- spec/support/feature/question.rb
+++ spec/support/feature/question.rb
@@ -9,28 +9,12 @@ module Feature
 
     def update_question(params)
       visit question_path(params[:question])
-      click_question_edit_link
+      click_on 'Edit'
       within '.form-edit-question' do
         fill_in 'Title', with: params[:title]
         fill_in 'Body', with: params[:body]
         click_on 'Save'
       end
-    end
-
-    def click_question_edit_link
-      within '.question-remote-links' do
-        click_on 'Edit'
-      end
-    end
-
-    def click_question_delete_link
-      within '.question-remote-links' do
-        click_on 'Delete'
-      end
-    end
-
-    def question_delete_link
-      ".link-delete-question"
     end
   end
 end
