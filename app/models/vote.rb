b11ec952def150bd67523f87fe042e33f56cb4ce
rusdec <ruskidecko@gmail.com>
2018-05-01 17:29:59 +0300

    feature: add scopes for likes selection by type to vote model + testing

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,12 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  scope :vote_for, ->(question) { find_by(votable: question) }
-
   validates_uniqueness_of :votable_id, scope: [:votable_type, :user_id]
+
+  scope :likes_for, ->(votable) { where(votable: votable).where(value: true) }
+  scope :dislikes_for, ->(votable) { where(votable: votable).where(value: false) }
+
+  def self.vote_for(votable)
+    find_by(votable: votable)
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Vote, type: :model do
   it { should belong_to(:votable) }
   it { should belong_to(:user) }
 
-  describe '#uniqueness' do
+  describe 'validate uniqueness' do
     subject { build :vote }
 
     it { should validate_uniqueness_of(:votable_id).
@@ -18,5 +18,23 @@ RSpec.describe Vote, type: :model do
     create(:question_vote, votable: question, user: create(:user))
 
     expect(user.votes.vote_for(question)).to eq(vote)
+  end
+
+  describe 'select likes by type' do
+    let!(:question) { create(:question, user: create(:user)) }
+    before do
+      create(:question_vote, votable: question, user: create(:user), value: true)
+      2.times do
+        create(:question_vote, votable: question, user: create(:user), value: false)
+      end
+    end
+
+    it '#likes_for' do
+      expect(Vote.likes_for(question).count).to eq(1)
+    end
+
+    it '#dislikes_for' do
+      expect(Vote.dislikes_for(question).count).to eq(2)
+    end
   end
 end
