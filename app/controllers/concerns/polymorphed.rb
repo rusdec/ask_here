4554b6d272138261a291c2f0925bc7f95a7f045c
rusdec <ruskidecko@gmail.com>
2018-05-02 14:38:15 +0300

    refactoring: add concern polymorfed for vote controller

---
+++ app/controllers/concerns/polymorphed.rb
@@ -0,0 +1,21 @@
+module Polymorphed
+  extend ActiveSupport::Concern
+
+  included do
+    def set_resource
+      param = resource_param
+      return nil unless params[param]
+      instance_variable_set '@resource', resource_klass(param).find(params[param])
+    end
+
+    private
+
+    def resource_param
+      params.each { |param, _| return param if /_id$/.match(param) }
+    end
+
+    def resource_klass(param)
+      param.split('_')[0].classify.constantize
+    end
+  end
+end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,21 +1,24 @@
 class VotesController < ApplicationController
+  include Polymorphed
+
+  before_action :authenticate_user!
   before_action :set_vote, except: :create
   before_action :set_resource, only: :create
 
   def create
-    if current_user&.not_author_of?(@resource)
+    if current_user.not_author_of?(@resource)
       @vote = current_user.votes.build(votable: @resource,
                                        value: vote_params[:value])
-      @vote.save!
+      @vote.save
     end
   end
 
   def update
-    @result = @vote.update(vote_params) if current_user&.author_of?(@vote)
+    @result = @vote.update(vote_params) if current_user.author_of?(@vote)
   end
 
   def destroy
-    @vote.destroy if current_user&.author_of?(@vote)
+    @vote.destroy if current_user.author_of?(@vote)
   end
 
   private
@@ -26,13 +29,5 @@ class VotesController < ApplicationController
 
   def vote_params
     params.require(:vote).permit(:value)
-  end
-
-  def set_resource
-    if params[:question_id]
-      @resource = Question.find(params[:question_id])
-    else
-      @resource = Answer.find(params[:answer_id])
-    end
   end
 end
